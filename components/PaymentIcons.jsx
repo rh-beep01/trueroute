234a7b167d4eb81ba1a0a3c0f0b39d402506f06d
rh-beep01 <rh144798@gmail.com>
2026-08-31 15:41:03 +0600

    feat: Use exact uploaded logo images for Visa, Mastercard, Amex, Discover, Diners Club, JCB, Apple Pay, Google Pay

--- components/PaymentIcons.jsx
+++ components/PaymentIcons.jsx
@@ -1,109 +1,41 @@
 import React from 'react';
 
-export function VisaIcon({ className = "h-6 w-auto" }) {
-  return (
-    <svg className={className} viewBox="0 0 48 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="48" height="32" rx="4" fill="#FFFFFF" stroke="#E2E8F0" strokeWidth="1" />
-      <path d="M19.4 21.6h-2.7l1.7-10.4h2.7l-1.7 10.4zm11.5-10.1c-.6-.2-1.4-.4-2.5-.4-2.8 0-4.7 1.4-4.7 3.4 0 1.5 1.4 2.3 2.5 2.8 1.1.5 1.5.9 1.5 1.3 0 .7-.9 1.1-1.7 1.1-1.1 0-1.8-.2-2.7-.6l-.4-.2-.4 2.3c.7.3 1.9.6 3.2.6 3 0 4.9-1.4 4.9-3.6 0-1.2-.8-2.1-2.4-2.9-1-.5-1.6-.8-1.6-1.3 0-.4.5-.9 1.6-.9.9 0 1.6.2 2.1.4l.3.1.3-2.1zm8 0h-2.1c-.6 0-1.2.2-1.4.8l-4.1 9.6h2.9l.6-1.6h3.5l.3 1.6h2.5l-2.2-10.4zm-3.3 6.6l1.4-3.9.8 3.9h-2.2zm-18.4-6.6l-2.6 7.1-.3-1.4c-.5-1.7-2.1-3.6-3.9-4.5l2.5 9.2h2.9l4.3-10.4h-2.9z" fill="#1434CB" />
-      <path d="M9.1 11.2H4.8l-.1.4c3.4.8 5.7 2.9 6.6 5.4l-1-4.9c-.2-.7-.7-.9-1.2-.9z" fill="#F7B600" />
-    </svg>
-  );
-}
+export const PAYMENT_METHODS = [
+  { name: 'Visa', src: '/payments/visa.png', alt: 'Visa' },
+  { name: 'Mastercard', src: '/payments/mastercard.png', alt: 'Mastercard' },
+  { name: 'American Express', src: '/payments/amex.png', alt: 'American Express' },
+  { name: 'Discover', src: '/payments/discover.png', alt: 'Discover' },
+  { name: 'Diners Club', src: '/payments/diners.png', alt: 'Diners Club' },
+  { name: 'JCB', src: '/payments/jcb.png', alt: 'JCB' },
+  { name: 'Apple Pay', src: '/payments/applepay.png', alt: 'Apple Pay' },
+  { name: 'Google Pay', src: '/payments/googlepay.png', alt: 'Google Pay' },
+];
 
-export function MastercardIcon({ className = "h-6 w-auto" }) {
-  return (
-    <svg className={className} viewBox="0 0 48 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="48" height="32" rx="4" fill="#FFFFFF" stroke="#E2E8F0" strokeWidth="1" />
-      <circle cx="19" cy="16" r="8" fill="#EB001B" />
-      <circle cx="29" cy="16" r="8" fill="#F79E1B" />
-      <path d="M24 10.74a7.96 7.96 0 0 0-3 5.26 7.96 7.96 0 0 0 3 5.26 7.96 7.96 0 0 0 3-5.26 7.96 7.96 0 0 0-3-5.26z" fill="#FF5F00" />
-    </svg>
-  );
-}
+export function PaymentCardImage({ item, size = "md" }) {
+  const containerHeight = size === "sm" ? "h-7 min-w-[38px] px-1.5" : "h-8 min-w-[46px] px-2";
+  const imgHeight = size === "sm" ? "h-4 max-w-[36px]" : "h-5 max-w-[42px]";
 
-export function AmexIcon({ className = "h-6 w-auto" }) {
   return (
-    <svg className={className} viewBox="0 0 48 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="48" height="32" rx="4" fill="#016FD0" />
-      <path d="M12.5 19.8l-1.3-3.2h-2l-1.3 3.2H5.6l3.3-7.6h3.1l3.3 7.6h-2.8zm-2.3-5.2l-.7 1.8h1.4l-.7-1.8zm6.5 5.2v-7.6h3.3l1.8 4.7 1.8-4.7h3.3v7.6h-2.3v-4.8l-1.9 4.8h-1.8l-1.9-4.8v4.8h-2.3zm12.9 0v-7.6h6.7v2h-4.4v.9h4.1v1.9h-4.1v.9h4.5v1.9h-6.8zm11 0l-2.4-3.6 2.3-4h-2.8l-1.1 2.1-1.1-2.1h-2.8l2.3 3.9-2.4 3.7h2.8l1.2-2.1 1.2 2.1h2.8z" fill="#FFFFFF" />
-    </svg>
-  );
-}
-
-export function DiscoverIcon({ className = "h-6 w-auto" }) {
-  return (
-    <svg className={className} viewBox="0 0 48 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="48" height="32" rx="4" fill="#FFFFFF" stroke="#E2E8F0" strokeWidth="1" />
-      <path d="M6 18.5V13.5h3.2c2.1 0 3.3 1.2 3.3 2.5s-1.2 2.5-3.3 2.5H6zm1.7-1.3h1.4c1.1 0 1.8-.5 1.8-1.2s-.7-1.2-1.8-1.2H7.7v2.4zm6 1.3V13.5h1.7v5h-1.7zm3.4 0c-.2-.3-.3-.7-.3-1.1h1.6c0 .4.4.6.9.6.5 0 .8-.2.8-.5 0-.9-3.2-.3-3.2-2.3 0-.9.8-1.7 2.4-1.7s2.3.8 2.3 1.7h-1.6c0-.4-.3-.6-.7-.6-.5 0-.8.2-.8.5 0 .8 3.2.3 3.2 2.3 0 .9-.8 1.7-2.5 1.7-.8 0-1.7-.3-2.1-.6zm7.2.1c-1.8 0-3-1.2-3-2.6s1.2-2.6 3-2.6c1.1 0 1.9.5 2.4 1.2l-1.3.8c-.3-.5-.6-.7-1.1-.7-.8 0-1.3.6-1.3 1.3s.5 1.3 1.3 1.3c.5 0 .8-.2 1.1-.7l1.3.8c-.5.8-1.3 1.2-2.4 1.2zm6.2-.1c-1.7 0-2.8-1.1-2.8-2.5 0-1.4 1.1-2.5 2.8-2.5s2.8 1.1 2.8 2.5c0 1.4-1.1 2.5-2.8 2.5zm3.7 0l-2.1-5h1.9l1.2 3.2 1.2-3.2h1.8l-2.1 5h-1.9zm5.7 0V13.5h4.8v1.3h-3.1v.6h2.8v1.3h-2.8v.6h3.2v1.2h-4.9zm6.8 0V13.5h3.2c1.3 0 2.2.7 2.2 1.8 0 .8-.5 1.4-1.2 1.6l1.5 1.6h-2l-1.2-1.4h-.8v1.4h-1.7zm1.7-2.6h1.3c.5 0 .8-.3.8-.6s-.3-.6-.8-.6h-1.3v1.2z" fill="#231F20" />
-      <circle cx="24.3" cy="16" r="2.8" fill="#FF6000" />
-    </svg>
-  );
-}
-
-export function DinersClubIcon({ className = "h-6 w-auto" }) {
-  return (
-    <svg className={className} viewBox="0 0 48 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="48" height="32" rx="4" fill="#FFFFFF" stroke="#E2E8F0" strokeWidth="1" />
-      <circle cx="24" cy="16" r="9" fill="#0079BE" />
-      <path d="M24 8.5a7.5 7.5 0 0 0-4.5 13.5V10a7.5 7.5 0 0 1 4.5-1.5zm0 15a7.5 7.5 0 0 0 4.5-13.5V22a7.5 7.5 0 0 1-4.5 1.5z" fill="#FFFFFF" />
-    </svg>
-  );
-}
-
-export function JcbIcon({ className = "h-6 w-auto" }) {
-  return (
-    <svg className={className} viewBox="0 0 48 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="48" height="32" rx="4" fill="#FFFFFF" stroke="#E2E8F0" strokeWidth="1" />
-      {/* Blue */}
-      <rect x="10" y="8" width="8" height="16" rx="2" fill="#0060A9" />
-      <path d="M15.5 11v6.5c0 1.4-.7 2-1.9 2-1.1 0-1.7-.5-1.7-1.5h1.4c0 .4.2.6.5.6.3 0 .5-.2.5-.6V11h1.2z" fill="#FFFFFF" />
-      {/* Red */}
-      <rect x="20" y="8" width="8" height="16" rx="2" fill="#E60012" />
-      <path d="M26.5 12.8c-.3-.7-.9-1-1.7-1-1.2 0-2 .8-2 2.2s.8 2.2 2 2.2c.8 0 1.4-.4 1.7-1.1h-1.4c-.1.3-.4.4-.7.4-.6 0-.9-.4-.9-1.2 0-.8.3-1.2.9-1.2.3 0 .6.2.7.4h1.4v-.7z" fill="#FFFFFF" />
-      {/* Green */}
-      <rect x="30" y="8" width="8" height="16" rx="2" fill="#00873C" />
-      <path d="M32 11h2.2c.8 0 1.4.3 1.4 1 0 .5-.3.8-.7.9.5.1.8.5.8 1 0 .8-.7 1.1-1.5 1.1H32V11zm1.2 1.6h.9c.3 0 .5-.1.5-.4s-.2-.4-.5-.4h-.9v.8zm0 1.6h1c.3 0 .5-.1.5-.4s-.2-.4-.5-.4h-1v.8z" fill="#FFFFFF" />
-    </svg>
-  );
-}
-
-export function ApplePayIcon({ className = "h-6 w-auto" }) {
-  return (
-    <svg className={className} viewBox="0 0 48 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="48" height="32" rx="4" fill="#000000" />
-      <path d="M17.15 15.65c-.01-1.92 1.57-2.85 1.64-2.9-.9-1.3-2.29-1.48-2.78-1.5-1.19-.12-2.32.7-2.92.7-.6 0-1.53-.68-2.52-.66-1.3.02-2.5.76-3.17 1.92-1.35 2.34-.35 5.8 0.97 7.7 0.64.93 1.41 1.97 2.42 1.93 0.97-.04 1.34-.63 2.51-.63 1.17 0 1.5.63 2.52.61 1.03-.02 1.69-.93 2.33-1.86.74-1.07 1.04-2.12 1.06-2.17-.02-.01-2.04-.78-2.06-3.14zm-1.71-5.46c.53-.64.89-1.54.79-2.44-.77.03-1.69.51-2.24 1.15-.49.56-.91 1.47-.8 2.35.86.07 1.72-.42 2.25-1.06z" fill="#FFFFFF" />
-      <path d="M23.1 19.8V11.5h3.4c2 0 3.2 1.1 3.2 2.8 0 1.8-1.3 2.9-3.2 2.9h-1.9v2.6h-1.5zm1.5-4h1.7c1.1 0 1.8-.6 1.8-1.6 0-1-.7-1.6-1.8-1.6h-1.7v3.2zm6.6 4v-5.9h1.4v.9c.4-.7 1.2-1 2-1 1.5 0 2.4 1 2.4 2.6v3.4h-1.4v-3.3c0-1-.5-1.5-1.4-1.5-.8 0-1.5.6-1.5 1.6v3.2h-1.5zm8 2.4l1.5-4.4-2.6-6.1h1.6l1.8 4.6 1.7-4.6h1.6l-4.1 9.7c-.3.8-.8 1.1-1.6 1.1-.3 0-.6 0-.8-.1l-.1-.2h.9z" fill="#FFFFFF" />
-    </svg>
-  );
-}
-
-export function GooglePayIcon({ className = "h-6 w-auto" }) {
-  return (
-    <svg className={className} viewBox="0 0 48 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="48" height="32" rx="4" fill="#FFFFFF" stroke="#E2E8F0" strokeWidth="1" />
-      {/* Google G */}
-      <path d="M15.5 16.14c0-.38-.03-.74-.1-1.09h-4.4v2.07h2.52c-.11.58-.44 1.08-.93 1.41v1.17h1.5c.88-.81 1.41-2 1.41-3.56z" fill="#4285F4" />
-      <path d="M11 20.73c1.28 0 2.35-.42 3.13-1.15l-1.5-1.17c-.42.28-.96.45-1.63.45-1.25 0-2.31-.84-2.69-1.98H6.76v1.21c.8 1.59 2.44 2.64 4.24 2.64z" fill="#34A853" />
-      <path d="M8.31 16.88c-.1-.28-.15-.58-.15-.88s.05-.6.15-.88V13.9H6.76c-.33.65-.51 1.38-.51 2.1s.18 1.45.51 2.1l1.55-1.22z" fill="#FBBC05" />
-      <path d="M11 13.27c.7 0 1.32.24 1.81.71l1.36-1.36c-.82-.77-1.89-1.23-3.17-1.23-1.8 0-3.44 1.05-4.24 2.64l1.55 1.22c.38-1.14 1.44-1.98 2.69-1.98z" fill="#EA4335" />
-      {/* Pay text */}
-      <path d="M21.2 19.3V12.7h2.7c1.6 0 2.5.9 2.5 2.2 0 1.4-1 2.3-2.5 2.3h-1.5v2.1h-1.2zm1.2-3.2h1.4c.9 0 1.4-.4 1.4-1.2s-.5-1.2-1.4-1.2h-1.4v2.4zm5.3 3.2v-4.7h1.1v.7c.3-.5.9-.8 1.6-.8 1.2 0 1.9.8 1.9 2.1v2.7h-1.1v-2.6c0-.8-.4-1.2-1.1-1.2-.6 0-1.2.5-1.2 1.3v2.5h-1.2zm6.4 1.9l1.2-3.5-2.1-4.9h1.3l1.4 3.7 1.4-3.7h1.3l-3.3 7.8c-.3.6-.6.8-1.3.8-.2 0-.5 0-.7-.1l.1-.1h.6z" fill="#5F6368" />
-    </svg>
+    <div
+      className={`${containerHeight} bg-white rounded-md border border-slate-200 shadow-xs flex items-center justify-center transition-all hover:border-slate-300`}
+      title={item.name}
+    >
+      <img
+        src={item.src}
+        alt={item.alt}
+        className={`${imgHeight} w-auto object-contain`}
+        loading="lazy"
+      />
+    </div>
   );
 }
 
 export function PaymentCardsRow({ size = "md", className = "" }) {
-  const iconClass = size === "sm" ? "h-6 w-auto" : "h-7 w-auto";
   return (
     <div className={`flex items-center flex-wrap gap-2 ${className}`}>
-      <VisaIcon className={iconClass} />
-      <MastercardIcon className={iconClass} />
-      <AmexIcon className={iconClass} />
-      <DiscoverIcon className={iconClass} />
-      <DinersClubIcon className={iconClass} />
-      <JcbIcon className={iconClass} />
-      <ApplePayIcon className={iconClass} />
-      <GooglePayIcon className={iconClass} />
+      {PAYMENT_METHODS.map((item) => (
+        <PaymentCardImage key={item.name} item={item} size={size} />
+      ))}
     </div>
   );
 }
